26bd3bd9fb6c1328c85336cc6449c9f67bddd424
sndrs <a@snd.rs>
2016-06-16 20:25:30 +0100

    squash

--- src/index.js
+++ src/index.js
@@ -163,9 +163,7 @@ module.exports = postcss.plugin('postcss-atomised', ({json = path.resolve(proces
             }
         }
     }),
-    reporter({
-        clearMessages: true
-    }),
+    reporter({clearMessages: true}),
     unchainSelectors(),
     mergeRules(),
     longhand(),
