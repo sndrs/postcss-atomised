3627e4fb12a94ebd19b8c3278b7b041db564bfd8
sndrs <a@snd.rs>
2016-06-16 20:27:49 +0100

    move test file

--- test/test.js
+++ test/test.js
@@ -4,15 +4,15 @@ import {readFileSync, readdirSync} from 'fs';
 import junk from 'junk';
 import path from 'path';
 import postcss from "postcss";
-import atomised from './src';
+import atomised from '../src';
 import perfectionist from "perfectionist";
 
-const fixturePath = check => `./test/fixtures/${check}`;
+const fixturePath = check => `./fixtures/${check}`;
 
 
 
 const srcCSS = check => postcss([
-    atomised({json: path.resolve('./test/output/', `${check}.json`)}),
+    atomised({json: path.resolve('./output/', `${check}.json`)}),
     perfectionist({format: 'compact'})
 ]).process(readFileSync(`${fixturePath(check)}/src.css`, 'utf8'), {from: `${fixturePath(check)}/src.css`});
 
@@ -22,9 +22,9 @@ const expectedCSS = check => postcss([
 
 const expectedMap = check => require(`${fixturePath(check)}/expected.json`);
 
-readdirSync('test/fixtures').filter(junk.not).forEach(check => {
+readdirSync('fixtures').filter(junk.not).forEach(check => {
     test(check, t => srcCSS(check).then((result) => {
-        const json = require(`./test/output/${check}.json`);
+        const json = require(`./output/${check}.json`);
         t.is(result.css, expectedCSS(check).css);
         t.deepEqual(json, expectedMap(check));
     }));
