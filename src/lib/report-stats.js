8d6360c4b29cdca3b5f44a8161a28082bdb276c6
Alex Sanders <a@snd.rs>
2016-08-04 01:06:07 +0100

    pluralise messages

--- src/lib/report-stats.js
+++ src/lib/report-stats.js
@@ -1,11 +1,13 @@
 import chalk from 'chalk';
 import prettyBytes from 'pretty-bytes';
 
+const pluralise = (term, count) => count > 1 ? `${term}s` : term;
+
 export default (result, stats, colour, description) => {
     result.messages.push({
         type:    'atomised-src',
         plugin:  'postcss-atomised',
-        text:     chalk[colour](`${description}${stats.declarations.total} declarations in ${prettyBytes(stats.gzipSize)}.`)
+        text:     chalk[colour](`${description}${stats.declarations.total} ${pluralise('declaration', stats.declarations.total)} in ${prettyBytes(stats.gzipSize)}.`)
     });
 }
 
