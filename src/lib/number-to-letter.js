ec76903e8327cda9deb1d528161408dc387d058c
Alex Sanders <a@snd.rs>
2016-09-09 19:12:10 +0100

    update lint config

--- src/lib/number-to-letter.js
+++ src/lib/number-to-letter.js
@@ -2,5 +2,5 @@
 // I got it off SO #toptier
 // http://stackoverflow.com/a/32007970
 export default function numberToLetter(i) {
-    return (i >= 52 ? numberToLetter((i / 52 >> 0) - 1) : '') + 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[i % 52 >> 0];
+    return (i >= 52 ? numberToLetter((i / 52 >> 0) - 1) : '') + 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[i % 52 >> 0]; // eslint-disable-line no-bitwise
 }
